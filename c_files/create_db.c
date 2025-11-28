af73306c9ea8fc128d47c8fe571ff673897bacfa
Lamis McDowall-Rose <lamism@gmail.com>
2025-11-28 18:43:29 +0000

    Progress maybe

--- c_files/create_db.c
+++ c_files/create_db.c
@@ -1,179 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-enum grade_enum {
-    V0,
-    V1,
-    V2,
-    V3,
-    5,
-    6a,
-    6a+,
-    6b,
-    6b+,
-    E3,
-    E4,
-    E6
-};
-
-enum route_type_enum {
-    bouldering,
-    sport,
-    trad
-};
-
-enum crag_faces{
-    N,
-    S,
-    E,
-    W
-}
-
-// no duplicate ids
-// climber will be indexable, so you can use B trees on it, just to copy Psotgres, the other tables will just be arrays of structs
-// 'When you create an index on a table, PostgreSQL creates a separate B-tree data structure that allows for faster data retrieval based on the indexed columns''
-//
-struct climber {
-    int id // primary key 
-    char * name
-    char * email // check using regex before processing further
-}
-
-struct route_struct {
-    char * route_name;
-    char * crag_name
-    double latitude 
-    double longitude
-    enum type type_of_route //enums are the size of ints
-    enum grade grade_of_route //enums are the size of ints
-    //climber * list_of_climbers
-};
-
-struct crag_struct {
-    char * crag_name // primary key
-    double latitude 
-    double longitude
-    enum crag_faces crag_face //enums are the size of ints
-};
-
-//-------------------------------------
-//FIRST CLIMBERS AND ROUTES. LEAVE CRAGS TILL LATER 
-
-struct climber_struct * set_climbers(char *filename){
-    FILE * file_pointer = fopen(filename, "r");
-
-    int max_str_field_length = 64; // max length of a string field is 64 characters
-
-    int number_of_string_fields = 2;
-    int number_of_int_or_enum_fields = 2;
-    int number_of_double_fields = 2;
-
-    int total_number_of_fields = 6
-    
-    char route[
-        (max_str_field_length * number_of_string_fields) + 
-        (sizeof int * number_of_int_or_enum_fields) +
-        (sizeof double * number_of_double_fields)
-    ];
-    char route_name[max_str_field_length];
-    char crag_name[max_str_field_length];
-
-    struct route_struct * route_list;
-    // B tree initialed here
-    int row_num = 0;
-    if (file_pointer != NULL) {
-    }
-}
-
-struct route_struct * set_routes(char *filename){
-    FILE * file_pointer = fopen(filename, "r");
-
-    int max_str_field_length = 64; // max length of a string field is 64 characters
-
-    int number_of_string_fields = 2;
-    int number_of_int_or_enum_fields = 2;
-    int number_of_double_fields = 2;
-
-    int total_number_of_fields = 6;
-
-    struct route_struct * route_list;
-    // B tree initialed here
-    int row_num = 0;
-    if (file_pointer != NULL) {
-        // ok, we are gonna malloc every 10 lines, I don't think mallocing every line is sensible 
-        // allocate memory chunk for the route here to use the heap 
-        char route[
-            (max_str_field_length * number_of_string_fields) + 
-            (sizeof int * number_of_int_or_enum_fields) +
-            (sizeof double * number_of_double_fields)
-        ];
-        char route_name[max_str_field_length];
-        char crag_name[max_str_field_length];
-
-        while (fgets(route, sizeof route, file_pointer) != NULL) {
-            
-            // remove white space from line here ?
-            const char * column_names[]  = {"route_name", "crag_name", "latitude", "longitude", "type_of_route", "grade_of_route"};
-            char * current_column = strtok(route, ",");
-
-            int i = 0;
-            while (current_column != NULL) {
-                // if (i > total_number_of_fields){
-                //      ConsoleLog.Error("too many values in the row %d", row_num)
-                //}
-                if (column_names[i] == "route_name"){
-                    route_list[row_num].route_name = current_column;
-                } else if (column_names[i] == "crag_name") {
-                    route_list[row_num].crag_name = current_column;
-                } else if (column_names[i] == "latitude") {
-                    route_list[row_num].latitude = current_column;
-                } else if (column_names[i] == "longitude") {
-                    route_list[row_num].longitude = current_column;
-                } else if (column_names[i] == "type_of_route") {
-                    route_list[row_num].type_of_route = current_column;
-                } else if (column_names[i] == "grade_of_route") {
-                    route_list[row_num].grade_of_route = current_column;
-                }
-                printf(current_column);
-                printf(",");
-                current_column = strtok(NULL, ",");
-                i++;
-            }
-            printf("\n");
-            row_num ++;
-        }
-        
-    }
-    else {
-        printf("Unable to open file!");
-    }
-    fclose(file_pointer);
-    return route_list;
-}
-
-char * trim(char * string_to_trim){
+#include <stdbool.h>
+#include <ctype.h>
+#include "data_structures.h"
+/*
+char * trim(char * string_to_trim) {
     // trim start 
-    while(isspace(char * string_at_start)) {
-        string_at_start++;
+    int i = 0;
+    char * string_at_start = &string_to_trim[i];
+    while(isspace(string_at_start)) {
+        i++;
+        char * string_at_start = &string_to_trim[i];
     }
     // trim end
-    end = string_to_trim + strlen(string_to_trim) - 1;
+    int end = string_to_trim + strlen(string_to_trim) - 1;
     while(end > string_at_start && isspace((unsigned char)*end)) {
         end--;
     }
     // Write new null terminator character
     end[1] = '\0';
-    return str
+    return str;
+}
+*/
+
+struct TableMetadata * clear_table_space(int num_rows) {
+    size_t table_size = 1000 * num_rows; // size or row times number of rows
+    size_t total_size = sizeof(struct TableMetadata) + table_size;
+    printf("Table Meta Data size: %zu\n", sizeof(struct TableMetadata));
+    printf("Table size: %zu\n", table_size);
+    printf("Total allocation size: %zu\n", total_size);
+
+    // Allocate one contiguous block of memory.
+    struct TableMetadata * meta_data_pointer = malloc(total_size);
+
+    // ALWAYS check for allocation failure.
+    if (my_data == NULL) {
+        perror("Failed to allocate memory");
+        return 1;
+    }
+
+    // Set table initial size
+    meta_data_pointer->table_size = table_size;
 }
 
-int main(void)
-{
-    struct route_struct * route_list_final = set_routes("route_list.txt");
-    size_t num_items = 64/ sizeof(route_list_final);
-    printf("%d", sizeof(route_list_final));
-    printf("%d", num_items);
+struct TableMetadata * set_table(char * filename) {
+    int max_row_size = 1000;
+    FILE * file_pointer = fopen(filename, "r");
+    struct TableMetadata * table_metadata;
+    int number_of_malloc_calls = 1; 
+    if (file_pointer != NULL) {
+        table_metadata->table_name = filename;
+        int row_num = 0;
+        char row[max_row_size];
+        while (fgets(row, max_row_size, file_pointer) != NULL) {
+            // if you have less than 5000 (5 rows) of space left, allocate more by calling 
+            char * current_column = strtok(row, ",");
+            int col_num = 0;
+            while (current_column != NULL) {
+                //current_column = trim(current_column);
+                fflush("%s\n", current_column);
+                (table_metadata->table_rows[row_num]).values[col_num] = current_column;
+                current_column = strtok(NULL, ",");
+                fflush("%d\n", col_num);
+                col_num++;
+            }
+            if (row_num == 0){
+                table_metadata->num_columns = col_num;
+            }
+            fflush("%d\n", row_num);
+            row_num ++;
+        }
+    }
+    else {
+        printf("Unable to open file!");
+    }
+    fclose(file_pointer);
+    return table_metadata;
+}
 
-    //struct climber_struct * climber_list_final = set_routes("climbing_list.txt");
-   
+void clear_table(struct TableMetadata * table_to_clear) {
+     // Clean up after you are done
+    free(my_data);
+    my_data = NULL; // Good practice to prevent use-after-free.
+}
+
+int main(void) {
+    printf("Starting:\n");
+    struct TableMetadata * meta_data_pointer = clear_table_space(10);
+    set_table("../csv_files/Route_List.csv", meta_data_pointer);
+    clear_table(meta_data_pointer);
+    
 }
---
+++ c_files/data_structures.h
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+// Structure to represent a row of data in a table
+typedef struct TableRow {
+    char ** values; // Array of values for each column
+} TableRow;
+
+// Structure to represent metadata for a table
+typedef struct TableMetadata {
+    uint32_t num_columns; 
+    size_t table_size;
+    char * table_name;  
+    struct TableRow table_rows[]; // has to be at the end of the struct                 
+} TableMetadata;
+
+
+
